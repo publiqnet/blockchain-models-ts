36ea0371f69b7f68e12fd9f20a44f60daade8cfd
Sergey Chimanyan <chimanyans@gmail.com>
2019-01-22 15:22:02 +0400

    change

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,23 @@
-import {parceToModel} from "../mapper";
+import {parceToJson, parceToModel} from "../mapper";
 import {JSON_DATA} from "./test-data";
 
-for (let data in JSON_DATA) {
-    console.log(parceToModel(JSON.stringify(JSON_DATA[data])));
+
+
+const tetsModels = () =>{
+    console.log("===================start models===============")
+    for (let data in JSON_DATA) {
+        console.log(parceToModel(JSON.stringify(JSON_DATA[data])));
+    }
 }
+
+
+
+const tetsJson = () =>{
+    console.log("===================start json===============")
+    for (let data in JSON_DATA) {
+        let obj = parceToModel(JSON.stringify(JSON_DATA[data]));
+        console.log( parceToJson(obj), '\n');
+    }
+};
+tetsJson();
 
