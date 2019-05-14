56a1ad13174c2fdf4d6c9674a739f6cee4915c73
SonaBadalyan <sona.badalyan.1992@gmail.com>
2019-05-14 14:57:19 +0400

    cpp build

--- models/PubliqRewardType.ts
+++ models/PubliqRewardType.ts
@@ -2,18 +2,18 @@ export default class PubliqRewardType {
 
     public static readonly initial = 0;
     public static readonly miner = 1;
-    public static readonly channel = 2;
-    public static readonly storage = 3;
-    public static readonly author = 4;
+    public static readonly author = 2;
+    public static readonly channel = 3;
+    public static readonly storage = 4;
 
     static toString(param: number): string {
 
         switch (param) {
             case 0: return "initial";
             case 1: return "miner";
-            case 2: return "channel";
-            case 3: return "storage";
-            case 4: return "author";
+            case 2: return "author";
+            case 3: return "channel";
+            case 4: return "storage";
         }
     } 
 
@@ -22,9 +22,9 @@ export default class PubliqRewardType {
         switch (param) {
             case "initial": return 0;
             case "miner": return 1;
-            case "channel": return 2;
-            case "storage": return 3;
-            case "author": return 4;
+            case "author": return 2;
+            case "channel": return 3;
+            case "storage": return 4;
         }
     } 
 
